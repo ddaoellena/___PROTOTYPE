5e4c010324ab3b30eb67225329124609271880ee
ddaoellena <ddaoellena@gmail.com>
2019-06-14 14:42:53 +0200

    Update 0-memes.js

--- assets/data/0-memes.js
+++ assets/data/0-memes.js
@@ -2,39 +2,34 @@ var pepe = {
   name : "Pepe the frog",
   toString : "pepe",
   id :0,
-  x: 300,
-  y: 300
+  nodeClass: ""
 }
 
 var godEmperor = {
   name : "God Emperor Trump",
   toString : "godEmperor",
   id : 1,
-  x: 300,
-  y: 300
+  nodeClass: ""
 }
 
 var yangGang = {
   name : "Yang Gang",
   toString : "yangGang",
   id :2,
-  x: 300,
-  y: 300
+  nodeClass: ""
 }
 
 var pizzagate = {
   name : "Pizzagate",
   toString : "pizzagate",
   id :3,
-  x: 300,
-  y: 300
+  nodeClass: ""
 }
 
 var risitas = {
   name : "Risitas",
   toString : "risitas",
   id :4,
-  x: 300,
-  y: 300
+  nodeClass: ""
 }
 var allMemes= [pepe, godEmperor, yangGang, pizzagate, risitas];
